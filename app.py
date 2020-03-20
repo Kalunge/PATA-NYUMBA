553a12135284061bb527a9c9f0929a09a4e4584c
Kalungez <muthomititus+github@gmail.com>
2020-03-20 21:20:07 +0300

    add the decorator for creating tables

--- app.py
+++ app.py
@@ -2,17 +2,28 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 from db import db
+
+
 from configs.config import Development
+from security import authenticate, identity
+from resources.landlord import LandLord, LandLordList
 
 app = Flask(__name__)
 db.init_app(app)
 
 app.config.from_object(Development)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 api = Api(app)
 
-jwt = JWT(app)
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(LandLordList, '/landlords')
+api.add_resource(LandLord, '/landlord')
 
 
 if __name__ == "__main__":
